6a8ca21a07a6b8ddfcb30b7fdbcee22461067009
Svilen Yanovski <svilen@yanovski.bg>
2021-04-26 01:09:43 +0300

    stopwatch

--- src/main/webapp/app/config/font-awesome-icons.ts
+++ src/main/webapp/app/config/font-awesome-icons.ts
@@ -37,6 +37,10 @@ import {
   faUsers,
   faUsersCog,
   faWrench,
+  faPlay,
+  faQuestion,
+  faChartLine,
+  faStopwatch,
   // jhipster-needle-add-icon-import
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -79,5 +83,9 @@ export const fontAwesomeIcons = [
   faUsers,
   faUsersCog,
   faWrench,
+  faPlay,
+  faQuestion,
+  faChartLine,
+  faStopwatch,
   // jhipster-needle-add-icon-import
 ];
--- src/main/webapp/app/home/start-quiz/start-quiz.component.ts
+++ src/main/webapp/app/home/start-quiz/start-quiz.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from 'app/entities/quiz/service/quiz.service';
 import { ActivatedRoute } from '@angular/router';
+import Timeout = NodeJS.Timeout;
 
 @Component({
   selector: 'jhi-start-quiz',
@@ -9,10 +10,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class StartQuizComponent implements OnInit {
   code!: string;
+  timeLimit!: number;
+  interval!: Timeout;
 
   constructor(protected quizService: QuizService, protected route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.code = this.route.snapshot.paramMap.get('code') ?? '';
+    this.timeLimit = 10;
+    this.startTimer();
+  }
+
+  startTimer(): void {
+    this.interval = setInterval(() => {
+      if (this.timeLimit > 0) {
+        this.timeLimit--;
+      } else {
+        this.timeLimit = 0;
+      }
+    }, 1000);
   }
 }
